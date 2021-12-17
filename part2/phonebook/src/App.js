8b082deb2f6f485cf2aa7eec58f72e62ca6faf9e
Cgard5424 <cgardner858@gmail.com>
2021-12-16 19:30:40 -0500

    Finished part 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,16 +3,19 @@ import React, { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas',
-      id: 'Arto Hellas' }
+      id: 'Arto Hellas',
+      number: '513-264-7933' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     //console.log(event.target.name)
     const nameObject = {
       name: newName,
-      id: newName
+      id: newName,
+      number: newNumber
     }
 
     if (!persons.some(person => person.name === newName)) {
@@ -29,6 +32,11 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -40,22 +48,28 @@ const App = () => {
           />
         </div>
         <div>
+          number: <input 
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(name =>
-        <Name key={name.id} name={name} />
+        <Numbers key={name.id} name={name} />
         )}
     </div>
   )
 }
 
-const Name = ({ name }) => {
+const Numbers = ({ name }) => {
   //console.log(name.id)
   return(
     <div>
-      {name.name}
+      {name.name} {name.number}
     </div>
   )
 }
