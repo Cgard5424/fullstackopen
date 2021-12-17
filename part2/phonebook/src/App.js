d2958e3c37101248286b571af430671de57af253
Cgard5424 <cgardner858@gmail.com>
2021-12-16 20:56:27 -0500

    Finished part 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      id: 'Arto Hellas',
-      number: '513-264-7933' }
-  ]) 
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addName = (event) => {
     event.preventDefault()
@@ -21,11 +19,16 @@ const App = () => {
     if (!persons.some(person => person.name === newName)) {
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     } 
     else {
       window.alert(`${newName} is already added to phonebook`)
     }
   }
+
+  const numbersToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.includes(newFilter) === true)
 
   const handleNameChange = (event) => {
     //console.log(event.target.value)
@@ -37,9 +40,30 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    //console.log(event.target.value.length)
+    if (event.target.value.length === 0) {
+      //
+      setShowAll(true)
+      setNewFilter('')
+    }
+    else {
+      setShowAll(false)
+      setNewFilter(event.target.value)
+    }
+    
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input
+          value={newFilter}
+          onChange={handleFilterChange}
+        />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input 
@@ -58,7 +82,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(name =>
+      {numbersToShow.map(name =>
         <Numbers key={name.id} name={name} />
         )}
     </div>
